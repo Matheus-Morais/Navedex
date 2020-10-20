3643489b1ae8ab4bb48eb0987c8c1a3a47c49a15
Matheus Morais <matheus.silva.morais01@gmail.com>
2020-10-20 10:19:19 -0300

    feat: estilização da pagina de signin

---
+++ src/pages/SignIn/styles.js
@@ -0,0 +1,43 @@
+import { Platform, Dimensions } from 'react-native';
+import styled from 'styled-components/native';
+
+import Background from '../../components/Background';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+const width = Dimensions.get('window').width;
+const height = Dimensions.get('window').height;
+
+export const Container = styled(Background)`
+`;
+
+export const KeyboardContainer = styled.KeyboardAvoidingView.attrs({
+    enabled: Platform.os === 'ios',
+    behavior: 'padding',
+})`
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const Logo = styled.Image`
+    height: ${height * 0.25}px;
+    width: ${width * 0.5}px;
+`;
+
+export const Form = styled.View`
+    width: ${width}px;
+    height: ${height * 0.6}px;
+`;
+
+export const ItemForm = styled.View`
+    margin: ${height * 0.035}px 16px 0 16px;
+`;
+
+export const Label = styled.Text``;
+
+export const FormInput = styled(Input)`
+`;
+
+export const SubmitButton = styled(Button)`
+`;
