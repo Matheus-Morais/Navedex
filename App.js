11930695a043dd96a3a4d419f56bd1052136fe55
Matheus Morais <matheus.silva.morais01@gmail.com>
2020-10-20 15:28:22 -0300

    fix: adicionando redux a raiz do projeto

--- App.js
+++ App.js
@@ -1,16 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
 import 'react-native-gesture-handler'
 
-import App from './src/app';
 import './src/utils/ReactotronConfig'
+import { store, persistor } from './src/store';
+import App from './src/app';
 
 export default function Index() {
     return (
-        <>
-            <App />
-            <StatusBar barStyle="dark-content" backgroundColor="#E5E5E5" />
-        </>
+        <Provider store={store}>
+            <PersistGate persistor={persistor}>
+                <StatusBar barStyle="dark-content" backgroundColor="#E5E5E5" />
+                <App />
+            </PersistGate>
+        </Provider>
 
     );
 }
