85b458f623a838989102e315a511e3afb6183145
Matheus Morais <matheus.silva.morais01@gmail.com>
2020-10-20 15:28:46 -0300

    fix: buscando o valor de logado para mandar para as rotas

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Routes from './routes';
 
 export default function App() {
+    const dispatch = useDispatch();
+    const isSignedIn = useSelector(state => state.auth.signed)
+
     return (
-        <Routes />
+        <Routes isSignedIn={isSignedIn} />
     )
 }
