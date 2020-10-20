6220b8f7d23c33393ac73aecee5e2147fac61103
Matheus Morais <matheus.silva.morais01@gmail.com>
2020-10-20 09:26:48 -0300

    feat: criação do app principal e das rotas

---
+++ src/routes.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const { Navigator, Screen } = createStackNavigator();
+
+import SignIn from './pages/SignIn';
+
+export default ({ isSignedIn = false, phoneVerified = false }) => {
+    const stackNavigationOption = {
+        headerTransparent: false,
+        headerTintColor: "#000",
+        headerTitleStyle: {
+            fontWeight: "bold",
+        },
+        headerTitleAlign: "center",
+        headerStyle: {
+            backgroundColor: "#FFF"
+        },
+        headerLeftContainerStyle: {
+            marginLeft: 20,
+        }
+    }
+
+    //Opções para um screen sem Header
+    const ScreenOptionNoHeader = {
+        headerShown: false
+    }
+
+    return (
+        <NavigationContainer>
+            <Navigator screenOptions={stackNavigationOption}>
+                <Screen
+                    name="SignIn"
+                    component={SignIn}
+                    options={ScreenOptionNoHeader}
+                />
+            </Navigator>
+        </NavigationContainer>
+    )
+}
