477f92769f036513f5d00ed9879923a45af4508f
Matheus Morais <matheus.silva.morais01@gmail.com>
2020-10-20 10:19:29 -0300

    fix: adicionando os estilos a pagina

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,13 +1,71 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useRef, useState } from 'react';
 
-// import { Container } from './styles';
+import logo from '../../assets/Logo.png';
+
+import {
+    Container,
+    KeyboardContainer,
+    Logo,
+    Form,
+    ItemForm,
+    Label,
+    FormInput,
+    SubmitButton
+} from './styles';
 
 const SignIn = () => {
+    const passwordRef = useRef()
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    async function handleSubmit() {
+        setLoading(!loading)
+        return
+    }
+
     return (
-        <View>
-            <Text>Hello SignIn</Text>
-        </View>
+        <Container>
+            <KeyboardContainer>
+
+                <Logo source={logo} resizeMode="contain" />
+
+                <Form>
+                    <ItemForm>
+                        <Label>E-mail</Label>
+                        <FormInput
+                            keyboardType="default"
+                            autoCorrect={false}
+                            autoCapitalize="none"
+                            placeholder="E-mail"
+                            returnKeyType="next"
+                            onSubmitEditing={() => passwordRef.current.focus()}
+                            value={email}
+                            onChangeText={setEmail}
+                        />
+                    </ItemForm>
+                    <ItemForm>
+                        <Label>Senha</Label>
+                        <FormInput
+                            placeholder="Senha"
+                            keyboardType="default"
+                            autoCorrect={false}
+                            ref={passwordRef}
+                            returnKeyType="send"
+                            secureTextEntry={true}
+                            onSubmitEditing={handleSubmit}
+                            value={password}
+                            onChangeText={setPassword}
+                        />
+                    </ItemForm>
+
+                    <ItemForm>
+                        <SubmitButton loading={loading} onPress={handleSubmit}>Entrar</SubmitButton>
+                    </ItemForm>
+                </Form>
+            </KeyboardContainer>
+        </Container>
     );
 }
 
