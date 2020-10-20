4284f79b5ed117a4c81f4d41eec13b27102f8ade
Matheus Morais <matheus.silva.morais01@gmail.com>
2020-10-20 09:26:20 -0300

    feat: criação da pagina de singin

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+const SignIn = () => {
+    return (
+        <View>
+            <Text>Hello SignIn</Text>
+        </View>
+    );
+}
+
+export default SignIn;
