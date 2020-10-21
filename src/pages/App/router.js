84980fe1e79b1c0cfbcc43e5e5c38ac21657a402
Matheus Morais <matheus.silva.morais01@gmail.com>
2020-10-21 20:54:05 -0300

    fix: configurando os assets das paginas

--- src/pages/App/router.js
+++ src/pages/App/router.js
@@ -1,25 +1,29 @@
 import React from 'react';
+import { Image, TouchableOpacity } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
-
 const { Navigator, Screen } = createStackNavigator()
 
 import Navers from './Navers';
 import Naver from './Naver';
 import NewNaver from './NewNaver';
 
+import Logo from '../../assets/Logo.png';
+import Menu from '../../assets/menu.png';
+import Arrow from '../../assets/arrow_back_ios.png';
 
 function NaversRouter() {
     const stackNavigationOption = {
         headerTransparent: false,
-        // headerTintColor: "#000",
-        // headerTitleStyle: {
-        //     fontWeight: "bold",
-        // },
         headerTitleAlign: "center",
-        // headerStyle: {
-        //     backgroundColor: "#FFF"
-        // },
-        // backButtonInCustomView: true,
+        headerStyle: {
+            backgroundColor: "#E5E5E5"
+        },
+        headerLeftContainerStyle: {
+            marginLeft: 20,
+        },
+        headerTitle: () => (
+            <Image style={{ width: 125, height: 32 }} source={Logo} />
+        ),
     }
 
     return (
@@ -27,14 +31,35 @@ function NaversRouter() {
             <Screen
                 name="Navers"
                 component={Navers}
+                options={({ navigation }) => ({
+                    headerLeft: () => (
+                        <TouchableOpacity onPress={() => { navigation.openDrawer(); }}>
+                            <Image style={{ width: 25, height: 20 }} source={Menu} />
+                        </TouchableOpacity>
+                    )
+                })}
             />
             <Screen
                 name="Naver"
                 component={Naver}
+                options={({ navigation }) => ({
+                    headerLeft: () => (
+                        <TouchableOpacity onPress={() => { navigation.goBack(); }}>
+                            <Image style={{ width: 25, height: 20 }} source={Arrow} />
+                        </TouchableOpacity>
+                    )
+                })}
             />
             <Screen
                 name="NewNaver"
                 component={NewNaver}
+                options={({ navigation }) => ({
+                    headerLeft: () => (
+                        <TouchableOpacity onPress={() => { navigation.goBack(); }}>
+                            <Image style={{ width: 25, height: 20 }} source={Arrow} />
+                        </TouchableOpacity>
+                    )
+                })}
             />
         </Navigator>
     )
