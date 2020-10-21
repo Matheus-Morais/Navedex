e32c23ee4d71ec7bb08319cc2d2c5cf85cf3c2c1
Matheus Morais <matheus.silva.morais01@gmail.com>
2020-10-21 10:45:01 -0300

    feat: component de message em uma modal

---
+++ src/components/Message/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import Modal from '../Modal';
+
+import {
+    Container,
+    Content,
+    ContainerHeader,
+    ContainerClose,
+    LabelTitle,
+    Label,
+} from './styles';
+
+export default function Message({ visible, onRequestClose, Title, Message }) {
+    return (
+        <Modal visible={visible} onRequestClose={onRequestClose}>
+            <Container>
+                <Content>
+                    <ContainerHeader>
+                        <LabelTitle>{Title}</LabelTitle>
+
+                        <ContainerClose onPress={onRequestClose}>
+                            <LabelTitle>X</LabelTitle>
+                        </ContainerClose>
+                    </ContainerHeader>
+                    <Label>{Message}</Label>
+                </Content>
+            </Container>
+        </Modal>
+    );
+}
+
---
+++ src/components/Message/styles.js
@@ -0,0 +1,53 @@
+import styled from 'styled-components/native';
+import { Dimensions } from 'react-native';
+
+const { height, width } = Dimensions.get("window");
+
+export const Container = styled.View`
+    width: 100%;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const Content = styled.View`
+    background: #fff;
+    width: 90%;
+    height: 15%;
+
+    padding: 0 24px 0 24px;
+
+    justify-content: center;
+`;
+
+export const ContainerHeader = styled.View`
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    margin-bottom: 16px;
+`;
+
+export const ContainerClose = styled.TouchableOpacity`
+`;
+
+export const LabelTitle = styled.Text`
+    font-style: normal;
+    font-weight: 600;
+    font-size: 22px;
+    line-height: 32px;
+
+    text-align: left;
+
+    color: #212121;
+`;
+
+export const Label = styled.Text`
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 24px;
+
+    text-align: left;
+
+    color: #212121;
+`;
