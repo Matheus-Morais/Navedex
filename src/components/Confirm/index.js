59c49bc31f9c2518f720aa6cf4ac4917cd6e3408
Matheus Morais <matheus.silva.morais01@gmail.com>
2020-10-21 16:49:45 -0300

    feat: component para apagar um naver

---
+++ src/components/Confirm/index.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+
+import Modal from '../Modal';
+import Message from '../Message'
+import api from '../../services/api';
+
+import {
+    Container,
+    Content,
+    ContainerHeader,
+    ContainerClose,
+    LabelTitle,
+    Label,
+    ContainerButtons,
+    CancelButton,
+    DeleteButton
+} from './styles';
+
+export default function Confirm({ visible, onRequestClose, idNaver }) {
+    const [loading, setLoading] = useState(false)
+
+    const [modalMessageVisible, setModalMessageVisible] = useState(false)
+    const [modalTitle, setModalTitle] = useState('Opps!')
+    const [modalMessage, setModalMessage] = useState('Ocorreu um erro desconhecindo, tente novamente mais tarde.')
+
+    async function handleDelete() {
+        setLoading(true)
+        try {
+            await api.delete(`navers/${idNaver}`)
+        } catch (error) {
+            //Se der algum erro na api, retorna uma mensagem para tentar novamente mais tarde
+            toggleModalMessageOpen()
+            setLoading(false)
+            return
+        }
+        onRequestClose()
+        setLoading(false)
+        return
+    }
+
+    function toggleModalMessageOpen() {
+        setModalMessageVisible(true)
+        return
+    }
+
+    //Função que fecha a modal
+    function toggleModalMessageClose() {
+        setModalMessageVisible(false)
+        return
+    }
+
+    return (
+        <Modal visible={visible} onRequestClose={onRequestClose}>
+            <Container>
+                <Content>
+                    <ContainerHeader>
+                        <LabelTitle>Excluir naver</LabelTitle>
+
+                        <ContainerClose onPress={onRequestClose}>
+                            <LabelTitle>X</LabelTitle>
+                        </ContainerClose>
+                    </ContainerHeader>
+                    <Label>Tem certeza que deseja excluir este naver?</Label>
+
+                    <ContainerButtons>
+                        <CancelButton color={'#212121'} onPress={onRequestClose}>Cancelar</CancelButton>
+
+                        <DeleteButton onPress={handleDelete} loading={loading}>Excluir</DeleteButton>
+                    </ContainerButtons>
+                </Content>
+            </Container>
+            <Message visible={modalMessageVisible} onRequestClose={toggleModalMessageClose} Title={modalTitle} Message={modalMessage} />
+        </Modal>
+    );
+}
+
---
+++ src/components/Confirm/styles.js
@@ -0,0 +1,69 @@
+import styled from 'styled-components/native';
+import { Dimensions } from 'react-native';
+import Button from '../Button';
+
+const height = Dimensions.get('window').height;
+
+export const Container = styled.View`
+    width: 100%;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const Content = styled.View`
+    background: #fff;
+    width: 90%;
+    height: 25%;
+
+    padding: 0 24px 0 24px;
+`;
+
+export const ContainerHeader = styled.View`
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    margin-bottom: 16px;
+    margin-top: 16px;
+`;
+
+export const ContainerClose = styled.TouchableOpacity`
+`;
+
+export const LabelTitle = styled.Text`
+    font-style: normal;
+    font-weight: 600;
+    font-size: 22px;
+    line-height: 32px;
+
+    text-align: left;
+
+    color: #212121;
+`;
+
+export const Label = styled.Text`
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 24px;
+
+    text-align: left;
+
+    color: #212121;
+`;
+
+export const ContainerButtons = styled.View`
+    flex-direction: row;
+    justify-content: space-between;
+
+    margin-top: ${height * 0.05}px;
+`;
+
+export const CancelButton = styled(Button)`
+    background: #fff;
+    width: 45%;
+`;
+
+export const DeleteButton = styled(Button)`
+    width: 45%;
+`;
