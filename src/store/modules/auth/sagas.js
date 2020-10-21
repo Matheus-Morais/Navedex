29737d2f09023c2721621f3cbc16f0294b3605f2
Matheus Morais <matheus.silva.morais01@gmail.com>
2020-10-21 09:03:24 -0300

    fix: sagas do token agora configura o Bearer

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -8,7 +8,7 @@ export function setToken({ payload }) {
     const { token } = payload.auth;
 
     if (token) {
-        api.defaults.headers.Authorization = `${token}`;
+        api.defaults.headers.Authorization = `Bearer ${token}`;
     }
 }
 
