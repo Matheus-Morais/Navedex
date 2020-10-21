76aaf677c7f73b838cc768fc8450e8f1c53c5144
Matheus Morais <matheus.silva.morais01@gmail.com>
2020-10-21 15:39:02 -0300

    fix: estilização dos navers

--- src/pages/App/Navers/index.js
+++ src/pages/App/Navers/index.js
@@ -1,26 +1,99 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
+
+import api from '../../../services/api';
+import EditIcon from '../../../assets/Edit-Icon.png';
+import DeleteIcon from '../../../assets/Delete-Icon.png';
 
 import {
     Container,
+    ContainerLoading,
+    Loading,
     Header,
     ContainerTitle,
     Title,
     AddButton,
+    List,
+    Naver,
+    ContainerImage,
+    Avatar,
+    Label,
+    ContainerOptions,
+    Touchable,
+    Icon
 } from './styles';
 
-const Navers = () => {
-    return (
-        <Container>
+export default function Navers({ navigation }) {
+    const [loading, setLoading] = useState(true)
+
+    const [navers, setNavers] = useState([])
+
+    //Função que busca os Navers
+    async function getNavers() {
+        setLoading(true)
+
+        let response
+        try {
+            response = await api.get('/navers')
+        } catch (error) {
+            setLoading(false)
+            return
+        }
+        setLoading(false)
+        setNavers(response.data)
+        return
+    }
+
+    //Essa função coloca o header dentro da lista, assim mantendo um efeito de scroll
+    function renderHeader() {
+        return (
             <Header>
                 <ContainerTitle>
                     <Title>Navers</Title>
                 </ContainerTitle>
 
-                <AddButton onPress={() => { }}>Adicionar naver</AddButton>
+                <AddButton onPress={() => { navigation.navigate('NewNaver') }}>Adicionar naver</AddButton>
             </Header>
+        )
+    }
+
+    useEffect(() => {
+        getNavers()
+    }, [])
+
+    return (
+        <Container>
+            {loading ?
+                <ContainerLoading>
+                    <Loading size='large' color="#000" />
+                </ContainerLoading> :
+
+                <List
+                    data={navers}
+                    ListHeaderComponent={renderHeader}
+                    keyExtractor={naver => String(naver.id)}
+                    renderItem={({ item: naver }) => (
+                        <Naver>
+                            <ContainerImage>
+                                <Avatar source={{ uri: naver.url }} resizeMode="contain" />
+                            </ContainerImage>
+                            <Label isBold>{naver.name}</Label>
+                            <Label>{naver.job_role}</Label>
+                            <ContainerOptions>
+                                <Touchable onPress={() => { }}>
+                                    <Icon source={DeleteIcon} resizeMode="contain" />
+                                </Touchable>
+
+                                <Touchable onPress={() => { }}>
+                                    <Icon source={EditIcon} resizeMode="contain" />
+                                </Touchable>
+                            </ContainerOptions>
+                        </Naver>
+                    )}
+                />
+
+            }
+
         </Container>
     );
 }
-
-export default Navers;
--- src/pages/App/Navers/styles.js
+++ src/pages/App/Navers/styles.js
@@ -44,3 +44,60 @@ export const Logo = styled.Image`
     height: ${height * 0.25}px;
     width: ${width * 0.5}px;
 `;
+
+export const ContainerLoading = styled.View`
+    width: 100%;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const Loading = styled.ActivityIndicator``;
+
+export const List = styled.FlatList.attrs({
+    showsVerticalScrollIndicator: false,
+    numColumns: 2,
+})`
+    margin-right: 9px;
+    margin-left: 9px;
+`;
+
+export const Naver = styled.View`
+    margin-top: 18px;
+    margin-bottom: 18px;
+
+    margin-right: 9px;
+    margin-left: 9px;
+`;
+
+export const ContainerImage = styled.View`
+    height: ${height * 0.22}px;
+    width: ${height * 0.22}px;
+`;
+
+export const Avatar = styled.Image`
+    height: ${height * 0.22}px;
+    width: ${height * 0.22}px;
+`;
+
+export const Label = styled.Text`
+    font-weight: ${props => props.isBold ? 'bold' : 'normal'};
+    font-size: 14px;
+    line-height: 20px;
+
+    color: #212121;
+`;
+
+export const ContainerOptions = styled.View`
+    margin-top: 5px;
+    flex-direction: row;
+`;
+
+export const Touchable = styled.TouchableOpacity`
+    margin-right: 5px;
+`;
+
+export const Icon = styled.Image`
+    height: 25px;
+    width: 25px;
+`;
